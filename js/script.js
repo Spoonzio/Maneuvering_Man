e1c71c100eff5dc86da45def947f7028c673cc5f
Rahim Askarzadeh <raskarzadeh1@my.bcit.ca>
2020-03-07 16:10:50 -0800

    Added the leaderboard.. Score incerements by one everytime a user guesses a word time.

--- js/script.js
+++ js/script.js
@@ -10,12 +10,41 @@ let userGuess = [];
 // Limbs
 let health = 6;
 
+// Score 
+
+let score = 0;
+let db;
+
+
+function initializeFirebase(){
+
+    // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAiIjqL3c9I3PKT7iFR9poW9pYabAND_ss",
+    authDomain: "hangman-41fa7.firebaseapp.com",
+    databaseURL: "https://hangman-41fa7.firebaseio.com",
+    projectId: "hangman-41fa7",
+    storageBucket: "hangman-41fa7.appspot.com",
+    messagingSenderId: "278759498076",
+    appId: "1:278759498076:web:3002181edab4e4b9f4a199"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+    db = firebase.firestore();
+
+}
+
 //
 // Inititator: Populate game word as array with random word
 //
 function init(){
+    initializeFirebase();
+    getLeaderboard();
+
     let randIndex = parseInt(Math.random()*wordList.length);
     gameWord = wordList[randIndex].split("");
+    
+    
 }
 
 //
@@ -61,6 +90,17 @@ function updateUserWord(letter){
         console.log("health: " + health)
     }
 
+// COMPARE BOTH ARRAYS TO SEE IF USER GUESS THE WORD CORRECTLY
+// IF TRUE, POST SCORE TO SCOREBOARD ON FIREBASE
+
+  if (gameWord.toString() == userGuess.toString()){
+      console.log("Score ++");
+      score++;
+      saveScore();
+
+  }
+
+
     showWord()
 }
 
@@ -69,6 +109,53 @@ function updateUserWord(letter){
 //
 function showWord(){
     console.log(userGuess.join(" "));
+   
+}
+
+// ADD HIGHSCORE TO FIREBASE 
+function saveScore(){
+    
+ let nameTextField = document.getElementById("name");
+    // Add a new document with a generated id.
+ db.collection("scores").add({
+    name: nameTextField.value,
+    score: score
+})
+.then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+})
+.catch(function(error) {
+    console.error("Error adding document: ", error);
+}); 
+
+}
+
+function getLeaderboard(){
+    let leaderboard =  document.getElementById("leaderboard");
+
+    db.collection("scores").orderBy('score', 'desc').limit(3).get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            // doc.data() is never undefined for query doc snapshots
+            if (doc.exists) {
+                console.log("Document data:", doc.data());
+                let a = document.createElement("p");
+                
+                a.innerHTML = doc.data().name + "&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;" + doc.data().score;
+                
+                leaderboard.append(a);
+
+                
+            } else {
+                // doc.data() will be undefined in this case
+                console.log("No such document!");
+            }
+        })
+    });
+    
+    
+       
+        
+
 }
 
 init()
